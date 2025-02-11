41eeae1b26ec6214b016d6bc2eadeae32d1e9cd1
Patrick Deuley <patrick.deuley@gmail.com>
2025-02-11 10:52:32 -0600

    fixes!

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',  // Enable static HTML export
+    images: {
+        unoptimized: true  // Required for static export
+    }
+}
+
+module.exports = nextConfig 
